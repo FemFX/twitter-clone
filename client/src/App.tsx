88ec94681ac19546bb158f75caf359176bc5eec0
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-27 08:05:07 +0300

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Switch, Route, useHistory } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import { fetchUserData } from "./redux/user/action";
+import { selectIsAuth, selectUserStatus } from "./redux/user/selectors";
+import { Loading } from "./redux/tweets/state";
+import { Twitter } from "@material-ui/icons";
+
+function App() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const isAuth = useSelector(selectIsAuth);
+  const loadingStatus = useSelector(selectUserStatus);
+  const isReady =
+    loadingStatus !== Loading.NEVER && loadingStatus !== Loading.LOADING;
+  useEffect(() => {
+    dispatch(fetchUserData());
+  }, [dispatch]);
+  useEffect(() => {
+    if (!isAuth && isReady) {
+      history.push("/signin");
+    } else {
+      history.push("/home");
+    }
+  }, [isAuth, isReady, history]);
+
+  if (!isReady) {
+    return (
+      <div className="center">
+        <Twitter color="primary" style={{ width: 100, height: 100 }} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/signin" component={Signin} />
+        <Route path="/home" component={Home} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
