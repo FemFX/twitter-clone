a3d2797347ef6a1bf78d036588e109e2ddb9d611
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-27 08:05:44 +0300

    Create index.tsx

---
+++ client/src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./redux/store";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
