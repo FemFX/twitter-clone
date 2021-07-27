b91ebe79db4a4930b935398425159815750c25a8
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-27 08:08:44 +0300

    Create Tweet.ts

---
+++ server/models/Tweet.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+
+const TweetSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const TweetModel = model("Tweet", TweetSchema);
