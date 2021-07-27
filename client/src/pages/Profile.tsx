ff7b2fd82ab2284d7da0cd1fd19e2fa038104d11
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-27 10:27:27 +0300

    Create Profile.tsx

---
+++ client/src/pages/Profile.tsx
@@ -0,0 +1,30 @@
+import {
+  Grid,
+  Paper,
+  Typography,
+  makeStyles,
+  Container,
+  TextField,
+  CircularProgress,
+} from "@material-ui/core";
+
+export const useStyles = makeStyles(() => ({
+  wrapper: {
+    height: "100vh",
+  },
+}));
+
+const Profile: React.FC = (): React.ReactElement => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.wrapper}>
+      <Grid container spacing={3}>
+        <Grid item sm={8} md={6}>
+          <h1>Profile</h1>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Profile;
