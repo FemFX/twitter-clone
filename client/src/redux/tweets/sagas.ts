50990b2deb15a44f4e79c5f3f28b40e131ab8a47
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-28 13:45:51 +0300

    Update sagas.ts

--- client/src/redux/tweets/sagas.ts
+++ client/src/redux/tweets/sagas.ts
@@ -6,6 +6,8 @@ import {
   FetchAddTweetActionInterface,
   addTweet,
   setAddForm,
+  removeTweet,
+  RemoveTweetActionInterface,
 } from "./action";
 import { TweetsApi } from "../../services/api/tweets";
 import { ITweet, Loading, AddForm } from "./state";
@@ -19,17 +21,28 @@ export function* fetchTweetsRequest(): Generator<any> {
   }
 }
 export function* fetchAddTweetRequest({
-  payload: text,
+  payload,
 }: FetchAddTweetActionInterface): Generator<any | ITweet> {
   try {
-    const item: any = yield call(TweetsApi.addTweet, text);
+    const item: any = yield call(TweetsApi.addTweet, payload);
     yield put(addTweet(item));
   } catch (err) {
     yield put(setAddForm(AddForm.ERROR));
+  }
+}
+export function* fetchRemoveTweetRequest({
+  payload,
+}: RemoveTweetActionInterface): Generator<any | ITweet> {
+  try {
+    yield call(TweetsApi.removeTweet, payload);
+    yield put(removeTweet(payload));
+  } catch (err) {
+    console.log(err);
   }
 }
 
 export function* tweetsSaga() {
   yield takeEvery(TweetActionsType.FETCH_TWEETS, fetchTweetsRequest);
   yield takeEvery(TweetActionsType.FETCH_ADD_TWEET, fetchAddTweetRequest);
+  yield takeEvery(TweetActionsType.REMOVE_TWEET, fetchRemoveTweetRequest);
 }
